c1667bcebbcafaf7d1001d0be8db38af661cfe9f
Elias <ebmm01@gmail.com>
2019-03-24 15:59:11 -0300

    Adicionando arquivo travis e mkdocs build

---
+++ mkdocs-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit
+
+rm -rf public
+
+# config
+git config --global user.email "${GH_USER_EMAIL}"
+git config --global user.name "${GH_USER_NAME}"
+git remote add gh-token "https://${GH_TOKEN}@github.com/Non-Devs/gerencial-API.git";
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+mkdocs gh-deploy --clean --remote-name gh-token;
+
+echo "Completed deploying documentation
