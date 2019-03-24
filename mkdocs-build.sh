8ccd232b5830f04e098c7133c9d3e6aa0032cf29
Elias <ebmm01@gmail.com>
2019-03-24 16:50:24 -0300

    Testando deploy mkdocs

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -6,7 +6,7 @@ rm -rf public
 # config
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
-git remote add gh-token "https://${GH_TOKEN}@github.com/Non-Devs/gerencial-API.git";
+git remote add gh-token "https://${GH_TOKEN}@github.com/fga-disciplinas/2019.1-Guia-Bolso.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy --clean --remote-name gh-token;
 
